c2bc2348613c891f4c67279bcd2bfd06f4186391
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-11 12:48:37 -0300

    Modificando a rota padrão

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -4,11 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CasaDoCodigo.Conrtollers
+namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
         public IActionResult Carrossel()
+        {
+            return View();
+        }
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        public IActionResult Resumo()
         {
             return View();
         }
