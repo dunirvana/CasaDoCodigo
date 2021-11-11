b39964677d13b330bf1bd335475e84e03a9b693f
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-11 19:15:30 -0300

    Ajustando a página de carrossel para mostrar os produtos do banco de dados

--- Aulas/Aula1/CasaDoCodigo/DataService.cs
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,15 +13,39 @@ namespace CasaDoCodigo
     public class DataService : IDataService
     {
         private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
 
-        public DataService(ApplicationContext contexto)
+        public DataService(ApplicationContext contexto,
+            IProdutoRepository produtoRepository)
         {
             this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
         }
 
         public void InicializaDB()
         {
-            contexto.Database.Migrate();
+            contexto.Database.EnsureCreated();
+
+            List<Livro> livros = GetLivros();
+            if (livros == null)
+                return;
+
+            produtoRepository.SaveProdutos(livros);
+
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            var filePath = "livros.json";
+            if (!File.Exists(filePath))
+                return null;
+
+            var json = File.ReadAllText(filePath);
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            
+            return livros;
         }
     }
+
+
 }
