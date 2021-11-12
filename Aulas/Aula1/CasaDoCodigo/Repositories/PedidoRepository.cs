6ed2db482f7c244a42065f1b25d38c115baaa570
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-12 18:48:22 -0300

    Obtendo/criando pedido

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,11 +1,12 @@
 ﻿using CasaDoCodigo.Models;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
     public interface IPedidoRepository
     {
-
+        Pedido GetPedido();
     }    
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -26,5 +27,20 @@ namespace CasaDoCodigo.Repositories
         {
             contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
         }
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet.Where(p => p.Id == pedidoId).SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+            }
+
+            return pedido;
+        }
+
     }
 }
