90751d0f40b602ddb920ff3ee7e055271138a8d7
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-15 11:13:39 -0300

    Adicionando e removendo itens

--- Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -5,7 +5,8 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-        void UpdateQuantidade(ItemPedido itemPedido);
+        ItemPedido GetItemPedido(int itemPedidoId);
+        void RemoveItemPedido(int itemPedidoId);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
@@ -13,19 +14,17 @@ namespace CasaDoCodigo.Repositories
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         { }
 
-        public void UpdateQuantidade(ItemPedido itemPedido)
+        public ItemPedido GetItemPedido(int itemPedidoId)
         {
-            var itemPedidoDB =
-                dbSet
-                 .Where(ip => ip.Id == itemPedido.Id)
-                 .SingleOrDefault();
-
-            if (itemPedidoDB != null)
-            {
-                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
-
-                contexto.SaveChanges();
-            }
+            return
+            dbSet
+            .Where(ip => ip.Id == itemPedidoId)
+            .SingleOrDefault();
         }
+        public void RemoveItemPedido(int itemPedidoId)
+        {
+            dbSet.Remove(GetItemPedido(itemPedidoId));
+        }
+
     }
 }
