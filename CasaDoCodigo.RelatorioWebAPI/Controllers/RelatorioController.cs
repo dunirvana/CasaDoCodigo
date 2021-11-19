9a6baf23304f92f3710a250445663622bd08d023
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-19 08:45:38 -0300

    Ajustes na api de relatório

--- CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
+++ CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +10,31 @@ namespace CasaDoCodigo.RelatorioWebAPI.Controllers
     [ApiController]
     public class RelatorioController : ControllerBase
     {
-        // GET: api/<RelatorioController>
+        private static readonly List<string> Relatorio =
+            new List<string>()
+            {
+                "Primeiro pedido",
+                "Segundo pedido"
+            };
+
+        // GET: api/Relatorio
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var item in Relatorio)
+            {
+                stringBuilder.AppendLine(item);
+            }
+
+            return stringBuilder.ToString();
         }
 
-        // GET api/<RelatorioController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<RelatorioController>
+        // POST: api/Relatorio
         [HttpPost]
         public void Post([FromBody] string value)
         {
-        }
-
-        // PUT api/<RelatorioController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RelatorioController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            Relatorio.Add(value);
         }
     }
 }
